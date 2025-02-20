49fca4bdcff448ac1103bf8442eddcd4a55696fb
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-02-21 01:37:12 +0500

    added ios

--- App.tsx
+++ App.tsx
@@ -1,43 +1,56 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Login from './src/screens/Login';
 import ChatList from './src/screens/ChatList';
 import ChatBox from './src/screens/ChatBox';
 import ProfileEdit from './src/screens/ProfileEdit';
-import { useEffect, useState } from 'react';
-import auth from "@react-native-firebase/auth"
-import firestore from "@react-native-firebase/firestore"
-import { Provider, useDispatch } from 'react-redux';
-import { setUser, removeUser } from './src/features/slices/userSlice'
+import {useEffect, useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import {Provider, useDispatch} from 'react-redux';
+import {setUser, removeUser} from './src/features/slices/userSlice';
 import store from './src/features/store';
-import { removeChatData } from './src/features/slices/chatDataSlice';
-import { removeChatUser } from './src/features/slices/chatUserSlice';
+import {removeChatData} from './src/features/slices/chatDataSlice';
+import {removeChatUser} from './src/features/slices/chatUserSlice';
 
 const ProfileStack = createNativeStackNavigator();
 
-function Profile({ navigation }) {
-
+function Profile({navigation}) {
   return (
     // Change to profile edit
-    <ProfileStack.Navigator initialRouteName='ProfileEdit'>
-      <ProfileStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-      <ProfileStack.Screen name="ProfileEdit" component={ProfileEdit} options={{ headerShown: false }} />
+    <ProfileStack.Navigator initialRouteName="ProfileEdit">
+      <ProfileStack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <ProfileStack.Screen
+        name="ProfileEdit"
+        component={ProfileEdit}
+        options={{headerShown: false}}
+      />
     </ProfileStack.Navigator>
   );
 }
 
 const ChatStack = createNativeStackNavigator();
 
-function Chat({ navigation }) {
-
-
+function Chat({navigation}) {
   return (
-    <ChatStack.Navigator initialRouteName='ChatList'>
-      <ChatStack.Screen name="ChatList" component={ChatList} options={{ headerShown: false }} />
-      <ChatStack.Screen name="ChatBox" component={ChatBox} options={{ headerShown: false }} />
+    <ChatStack.Navigator initialRouteName="ChatList">
+      <ChatStack.Screen
+        name="ChatList"
+        component={ChatList}
+        options={{headerShown: false}}
+      />
+      <ChatStack.Screen
+        name="ChatBox"
+        component={ChatBox}
+        options={{headerShown: false}}
+      />
     </ChatStack.Navigator>
   );
 }
@@ -45,100 +58,120 @@ function Chat({ navigation }) {
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
-
   const [isAuthenticated, setIsAuthenticated] = useState(null); // to show login screen when unauthenticated
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth().onAuthStateChanged(async (user) => {
+    const unsubscribe = auth().onAuthStateChanged(async user => {
       if (user) {
         try {
           // Fetching user data from "users" collection
-          const userSnapshot = await firestore().collection("users").doc(user.uid).get()
-          const userData = userSnapshot.data()
-          if (userData) {  // Ensure userData is not undefined
-            dispatch(setUser({
-              email: user.email,
-              id: user.uid,
-              username: userData.username,
-              name: userData.name,
-              bio: userData.bio,
-              avatar: userData.avatar,
-              lastSeen: userData.lastSeen
-            }))
-            console.log("Data fetched")
+          const userSnapshot = await firestore()
+            .collection('users')
+            .doc(user.uid)
+            .get();
+          const userData = userSnapshot.data();
+          if (userData) {
+            // Ensure userData is not undefined
+            dispatch(
+              setUser({
+                email: user.email,
+                id: user.uid,
+                username: userData.username,
+                name: userData.name,
+                bio: userData.bio,
+                avatar: userData.avatar,
+                lastSeen: userData.lastSeen,
+              }),
+            );
+            console.log('Data fetched');
           } else {
-            console.error("No user data found for this user! Ignore, If raised after signup");
+            console.error(
+              'No user data found for this user! Ignore, If raised after signup',
+            );
           }
           setIsAuthenticated(true);
-        }
-        catch (err) {
-          console.log(err)
+        } catch (err) {
+          console.log(err);
         }
       } else {
         setIsAuthenticated(false);
-        dispatch(removeUser())
-        dispatch(removeChatData())
-        dispatch(removeChatUser())
+        dispatch(removeUser());
+        dispatch(removeChatData());
+        dispatch(removeChatUser());
       }
     });
     return () => unsubscribe();
   }, []);
 
   if (isAuthenticated === null) {
-    return <View style={styles.loading}><Text style={styles.loadingText}>Loading...</Text></View>;
+    return (
+      <View style={styles.loading}>
+        <Text style={styles.loadingText}>Loading...</Text>
+      </View>
+    );
   }
 
   return (
-    <NavigationContainer>
-      {isAuthenticated ? (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === 'Profile') {
-                iconName = focused ? 'person' : 'person-outline';
-              } else if (route.name === 'Chat') {
-                iconName = focused ? 'chatbubble' : 'chatbubble-outline';
-              }
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarLabel: ({ focused }) => (
-              <Text style={{ fontSize: focused ? 15 : 14, fontWeight: 'bold', color: "gray" }}>
-                {route.name}
-              </Text>
-            ),
-          })}
-        >
-          <Tab.Screen name="Profile" component={Profile} />
-          <Tab.Screen name="Chat" component={Chat}/>
-        </Tab.Navigator>
-      ) : (
-        // If not authenticated, show the login screen
-        <ProfileStack.Navigator>
-          <ProfileStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        </ProfileStack.Navigator>
-      )}
-    </NavigationContainer>
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer>
+        {isAuthenticated ? (
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              headerShown: false,
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                if (route.name === 'Profile') {
+                  iconName = focused ? 'person' : 'person-outline';
+                } else if (route.name === 'Chat') {
+                  iconName = focused ? 'chatbubble' : 'chatbubble-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarLabel: ({focused}) => (
+                <Text
+                  style={{
+                    fontSize: focused ? 15 : 14,
+                    fontWeight: 'bold',
+                    color: 'gray',
+                  }}>
+                  {route.name}
+                </Text>
+              ),
+            })}>
+            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen name="Chat" component={Chat} />
+          </Tab.Navigator>
+        ) : (
+          // If not authenticated, show the login screen
+          <ProfileStack.Navigator>
+            <ProfileStack.Screen
+              name="Login"
+              component={Login}
+              options={{headerShown: false}}
+            />
+          </ProfileStack.Navigator>
+        )}
+      </NavigationContainer>
+    </SafeAreaView>
   );
-}
+};
 
 export default function App() {
   return (
     <Provider store={store}>
       <Main />
     </Provider>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   loading: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   loadingText: {
     fontSize: 25,
-  }
-})
+  },
+});
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,194 +1,279 @@
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native'
-import React, { useState } from 'react'
-import auth from "@react-native-firebase/auth"
-import firestore from "@react-native-firebase/firestore"
-import { useDispatch, useSelector } from 'react-redux';
-import { setUser } from '../features/slices/userSlice';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import {useDispatch, useSelector} from 'react-redux';
+import {setUser} from '../features/slices/userSlice';
 import SimpleToast from 'react-native-simple-toast';
 
 const Login = ({navigation}) => {
+  const [currState, setCurrState] = useState('Login');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false); // if true, button is disabled and TextInputs are not editable
 
-  const [currState, setCurrState] = useState("Login")
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [loading, setLoading] = useState(false) // if true, button is disabled and TextInputs are not editable
+  const defaultAvatar =
+    'https://firebasestorage.googleapis.com/v0/b/chat-app-e2ad2.appspot.com/o/images%2Favatar_icon.png?alt=media&token=2ae0cf20-afff-4f7f-9c3c-2b6d97f35fc8';
 
-  const defaultAvatar = 'https://firebasestorage.googleapis.com/v0/b/chat-app-e2ad2.appspot.com/o/images%2Favatar_icon.png?alt=media&token=2ae0cf20-afff-4f7f-9c3c-2b6d97f35fc8'
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const validateEmail = (email) => {
+  const validateEmail = email => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
   const signUp = async () => {
-
     if (!validateEmail(email)) {
-      SimpleToast.show("Please provide a valid email address", SimpleToast.SHORT);
-      return;
-    }
-    
-    if (password.length < 6) {
-      SimpleToast.show("Password should be at least 6 characters long", SimpleToast.SHORT);
+      SimpleToast.show(
+        'Please provide a valid email address',
+        SimpleToast.SHORT,
+      );
       return;
     }
 
-    if (username.trim() === "") {
-      SimpleToast.show("Username cannot be empty", SimpleToast.SHORT);
+    if (password.length < 6) {
+      SimpleToast.show(
+        'Password should be at least 6 characters long',
+        SimpleToast.SHORT,
+      );
+      return;
+    }
+
+    if (username.trim() === '') {
+      SimpleToast.show('Username cannot be empty', SimpleToast.SHORT);
       return;
     }
 
     try {
-
-      setLoading(true) 
+      setLoading(true);
       // create user in auth
-      const userCredentials = await auth().createUserWithEmailAndPassword(email, password)
-      const user = userCredentials.user
+      const userCredentials = await auth().createUserWithEmailAndPassword(
+        email.toLowerCase(),
+        password,
+      );
+      const user = userCredentials.user;
 
       // create user in "users" collection
-      await firestore().collection("users").doc(user.uid).set({
-        id: user.uid,
-        email: user.email,
-        username: username.toLowerCase(),
-        name: "",
-        avatar: defaultAvatar,
-        bio: "Hi there! I am using chat app",
-        lastSeen: Date.now(),
-      }).then(() => { console.log('User created!') })
+      await firestore()
+        .collection('users')
+        .doc(user.uid)
+        .set({
+          id: user.uid,
+          email: user.email,
+          username: username.toLowerCase(),
+          name: '',
+          avatar: defaultAvatar,
+          bio: 'Hi there! I am using chat app',
+          lastSeen: Date.now(),
+        })
+        .then(() => {
+          console.log('User created!');
+        });
 
-
-
-      // create "chats" collection for specific user (chat list) 
-      await firestore().collection("chats").doc(user.uid).set({
-        chatsData: []
-      }).then(() => { console.log("User's chats created!") })
+      // create "chats" collection for specific user (chat list)
+      await firestore()
+        .collection('chats')
+        .doc(user.uid)
+        .set({
+          chatsData: [],
+        })
+        .then(() => {
+          console.log("User's chats created!");
+        });
 
       // Saving data in RTK store after sign up
-      const userSnapshot = await firestore().collection("users").doc(user.uid).get();
+      const userSnapshot = await firestore()
+        .collection('users')
+        .doc(user.uid)
+        .get();
       const userData = userSnapshot.data();
-      if(userData){
-        dispatch(setUser({ 
-          email: user.email, 
-          id: user.uid, 
-          username: userData.username,
-          name: userData.name,
-          avatar: userData.avatar,
-          bio: userData.bio,
-          lastSeen: userData.lastSeen, 
-        }));
+      if (userData) {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            username: userData.username,
+            name: userData.name,
+            avatar: userData.avatar,
+            bio: userData.bio,
+            lastSeen: userData.lastSeen,
+          }),
+        );
       }
-      SimpleToast.show("Account created successfully!", SimpleToast.SHORT);
+      SimpleToast.show('Account created successfully!', SimpleToast.SHORT);
+    } catch (err) {
+      setLoading(false);
+      SimpleToast.show(
+        'Error creating account: ' + err.message,
+        SimpleToast.LONG,
+      );
+      console.log(err);
     }
-    catch (err) {
-      setLoading(false)
-      SimpleToast.show("Error creating account: " + err.message, SimpleToast.LONG);
-      console.log(err)
-    }
-  }
+  };
 
   const signIn = async () => {
-
     if (!validateEmail(email)) {
-      SimpleToast.show("Please provide a valid email address", SimpleToast.SHORT);
+      SimpleToast.show(
+        'Please provide a valid email address',
+        SimpleToast.SHORT,
+      );
       return;
     }
-    
+
     if (password.length < 6) {
-      SimpleToast.show("Password should be at least 6 characters long", SimpleToast.SHORT);
+      SimpleToast.show(
+        'Password should be at least 6 characters long',
+        SimpleToast.SHORT,
+      );
       return;
     }
 
     try {
-      setLoading(true)
-      const userCredentials = await auth().signInWithEmailAndPassword(email, password)
-      const user = userCredentials.user
-      const userSnapshot = await firestore().collection("users").doc(user.uid).get()
-      const userData = userSnapshot.data()
+      setLoading(true);
+      const userCredentials = await auth().signInWithEmailAndPassword(
+        email,
+        password,
+      );
+      const user = userCredentials.user;
+      const userSnapshot = await firestore()
+        .collection('users')
+        .doc(user.uid)
+        .get();
+      const userData = userSnapshot.data();
 
       // console.log("This is data:", userData)
-      SimpleToast.show("Logged in successfully!", SimpleToast.SHORT);
+      SimpleToast.show('Logged in successfully!', SimpleToast.SHORT);
+    } catch (err) {
+      setLoading(false);
+      SimpleToast.show(
+        err.code.split('/')[1].split('-').join(' '),
+        SimpleToast.SHORT,
+      );
+      console.log(err);
     }
-    catch (err) {
-      setLoading(false)
-      SimpleToast.show(err.code.split("/")[1].split("-").join(" "), SimpleToast.SHORT);
-      console.log(err)
-      
-    }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      {
-        currState === "Sign up"
-          ? (
-            <View style={styles.innerContainer}>
-              <Text style={styles.header}>{currState}</Text>
-              <TextInput style={styles.input} value={username} onChangeText={setUsername} placeholder='Username..' placeholderTextColor="gray" editable={!loading}/>
-              <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Email..' placeholderTextColor="gray" editable={!loading}/>
-              <TextInput style={styles.input} secureTextEntry value={password} onChangeText={setPassword} placeholder='Password..' placeholderTextColor="gray" editable={!loading}/>
-              <Button title={currState === 'Sign up' ? 'Create account' : 'Login now'} onPress={signUp} disabled={loading}/>
-              <Text style={styles.switch}>
-                Already have an account?{' '}
-                <Text onPress={() => setCurrState('Login')} style={styles.span}>
-                  Click here
-                </Text>
-              </Text>
-            </View>
-          )
-          : (
-            <View style={styles.innerContainer}>
-              <Text style={styles.header}>{currState}</Text>
-              <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Email..' placeholderTextColor="gray" editable={!loading}/>
-              <TextInput style={styles.input} secureTextEntry value={password} onChangeText={setPassword} placeholder='Password..' placeholderTextColor="gray" editable={!loading}/>
-              <Button title={currState === 'Sign up' ? 'Create account' : 'Login now'} onPress={signIn} disabled={loading}/>
-              <Text style={styles.switch}>
-                Already have an account?{' '}
-                <Text onPress={() => setCurrState('Sign up')} style={styles.span}>
-                  Click here
-                </Text>
-              </Text>
-            </View>
-          )
-      }
+      {currState === 'Sign up' ? (
+        <View style={styles.innerContainer}>
+          <Text style={styles.header}>{currState}</Text>
+          <TextInput
+            style={styles.input}
+            value={username}
+            onChangeText={setUsername}
+            placeholder="Username.."
+            placeholderTextColor="gray"
+            editable={!loading}
+          />
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email.."
+            placeholderTextColor="gray"
+            editable={!loading}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password.."
+            placeholderTextColor="gray"
+            editable={!loading}
+          />
+          <Button
+            title={currState === 'Sign up' ? 'Create account' : 'Login now'}
+            onPress={signUp}
+            disabled={loading}
+          />
+          <Text style={styles.switch}>
+            Already have an account?{' '}
+            <Text onPress={() => setCurrState('Login')} style={styles.span}>
+              Click here
+            </Text>
+          </Text>
+        </View>
+      ) : (
+        <View style={styles.innerContainer}>
+          <Text style={styles.header}>{currState}</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email.."
+            placeholderTextColor="gray"
+            editable={!loading}
+          />
+          <TextInput
+            style={styles.input}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password.."
+            placeholderTextColor="gray"
+            editable={!loading}
+          />
+          <Button
+            title={currState === 'Sign up' ? 'Create account' : 'Login now'}
+            onPress={signIn}
+            disabled={loading}
+          />
+          <Text style={styles.switch}>
+            Already have an account?{' '}
+            <Text onPress={() => setCurrState('Sign up')} style={styles.span}>
+              Click here
+            </Text>
+          </Text>
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#E8F0FE", 
+    backgroundColor: '#E8F0FE',
   },
   innerContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     gap: 20,
   },
   header: {
-    color: "black",
-    fontWeight: "bold",
-    textAlign: "center",
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
     fontSize: 30,
     marginBottom: 20,
   },
   input: {
-    width: "80%",
+    width: '80%',
     padding: 5,
     margin: 5,
     borderWidth: 1,
     borderRadius: 10,
-    color: "black",
+    color: 'black',
   },
   switch: {
     color: 'gray',
   },
   span: {
-    fontWeight: "bold",
-    color: "#079cff",
-  }
-})
+    fontWeight: 'bold',
+    color: '#079cff',
+  },
+});
 
-export default Login
+export default Login;
