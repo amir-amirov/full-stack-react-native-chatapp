4425fcb28d09a9d3d3afc4b10b2a0e28e1a8b2cc
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-03-12 19:55:50 +0500

    Added icons

--- android/app/src/main/java/com/jaiyqchat/MainActivity.kt
+++ android/app/src/main/java/com/jaiyqchat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.chatappmobile
+package com.jaiyqchat
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity
@@ -12,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ChatAppMobile"
+  override fun getMainComponentName(): String = "Jaiyq Chat"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
--- ios/JaiyqChatTests/JaiyqChatTests.m
+++ ios/JaiyqChatTests/JaiyqChatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ChatAppMobileTests : XCTestCase
+@interface JaiyqChatTests : XCTestCase
 
 @end
 
-@implementation ChatAppMobileTests
+@implementation JaiyqChatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
